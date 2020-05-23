1c39ba9ed76fa5295984462dc3548c80ef80db7a
Akhila <gayathreemunasinghe@gmail.com>
2020-05-23 21:14:51 +1200

    basic javascript setup for the project

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#score')
+  const startButton = document.querySelector('#start-button')
+  const width = 10
 
 })
